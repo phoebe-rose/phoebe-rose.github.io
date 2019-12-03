9c585a705bcd4418e0caa52ba29dcb957a995b46
Lily <lilyzh@umd.edu>
2019-12-02 21:58:04 -0500

    initial push

---
+++ ecriture.js
@@ -0,0 +1,99 @@
+var jours_ensoleilles = {
+   'Incipit' : 'incipit.txt',
+   'Repas parfait' : 'repas_parfait.txt',
+   'Couteau chinois' : 'couteau_chinois.txt',
+   'Derniers mots' : 'derniers_mots.txt'
+}
+
+var misc = {
+   'Stage Crew' : '#',
+   'M.' : '#'
+}
+
+var histoires = {
+   'Jeune garçon' : '#'
+}
+
+function render_menu_items(obj, klass) {
+   return Object.keys(obj).reduce(function (acc, title) {
+      return `${acc}<span class="${klass} file="${obj[title]}">${title}</span>`;
+   }, '');
+}
+
+$(document).ready(function() {
+
+   $('#content').css('display', 'flex');
+   $('#content').css('flex-direction', 'row');
+
+   // Populate writing menu
+
+   $('#writing_menu').append('<h1>Jours ensoleillés</h1>');
+   $('#writing_menu').append(render_menu_items(jours_ensoleilles, 'writing_menu_item'));
+
+   $('#writing_menu').append('<h1>Histoires</h1>');
+   $('#writing_menu').append(render_menu_items(histoires, 'writing_menu_item'));
+
+   $('#writing_menu').append('<h1>Misc.</h1>');
+   $('#writing_menu').append(render_menu_items(misc, 'writing_menu_item'));
+
+   // Change link color on hover
+   $('.writing_menu_item').css('cursor', 'pointer');
+   $('.writing_menu_item').on({
+      mouseenter: function () { $(this).addClass('writing_menu_item_hover'); },
+      mouseleave: function () { $(this).removeClass('writing_menu_item_hover'); }
+   });
+
+   // Populate writing display
+   $('.writing_menu_item').click(function() {
+      // Bold the clicked item
+      // $('.writing_menu_item').css('font-weight', 'normal');
+      // $(this).css('font-weight', 'bold');
+
+      $('.writing_menu_item').css('text-decoration', 'none');
+      $(this).css('text-decoration', 'underline dotted');
+
+      // TEST
+      // $('#writing_display').empty();
+      // $.ajax({
+      //   url: 'https://www.w3.org/TR/PNG/iso_8859-1.txt',
+      //   success: function(data) {
+      //     var title = $('<h1>Test piece</h1>').hide();
+      //     var text = $(`<p>${data}</p>`).hide()
+      //     $('#writing_display').append(title, text);
+      //     title.fadeIn();
+      //     text.fadeIn();
+      //   },
+      //   error: function(XMLHttpRequest, textStatus, errorThrown) {
+      //     var text = $(`<p>Content unavailable</p>`).hide()
+      //     $('#writing_display').append(text);
+      //     text.fadeIn();
+      //   }
+      // });
+
+      // Clear and populate display
+      // $.get(`./ecriture/${$(this).attr('file')}`, function(data) {
+      //    var title = $(`<h1>${$(this).text()}</h1>`).hide();
+      //    var text = $(`<p>${data}</p>`).hide()
+      //    $('#writing_display').append(title, text);
+      //    title.fadeIn();
+      //    text.fadeIn();
+      // });
+
+      $('#writing_display').empty();
+      $.ajax({
+        url: `./ecriture/${$(this).attr('file')}`,
+        success: function(data) {
+          var title = $(`<h1>${$(this).text()}</h1>`).hide();
+          var text = $(`<p>${data}</p>`).hide()
+          $('#writing_display').append(title, text);
+          title.fadeIn();
+          text.fadeIn();
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+          var text = $(`<p>Content unavailable</p>`).hide()
+          $('#writing_display').append(text);
+          text.fadeIn();
+        }
+      });
+   });
+});
---
+++ site.js
@@ -0,0 +1,53 @@
+var pages = {
+   'écriture' : 'ecriture.html',
+   'desseins' : 'desseins.html',
+   'pensées' : 'pensees.html',
+   'photos' : 'photos.html',
+   'livres' : 'livres.html',
+   'musique' : 'musique.html'
+}
+
+var text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet enim. Egestas diam in arcu cursus euismod quis. Blandit libero volutpat sed cras ornare arcu dui. Habitant morbi tristique senectus et netus. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Porta non pulvinar neque laoreet suspendisse interdum consectetur libero id. Elementum sagittis vitae et leo duis ut diam quam nulla. Phasellus vestibulum lorem sed risus ultricies. Ligula ullamcorper malesuada proin libero nunc. Vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa.
+
+Magna ac placerat vestibulum lectus mauris ultrices eros in. Non enim praesent elementum facilisis. Aliquet nibh praesent tristique magna sit amet purus gravida. Integer eget aliquet nibh praesent tristique magna. Risus sed vulputate odio ut enim blandit. Ornare lectus sit amet est placerat. Sed augue lacus viverra vitae congue. Vitae ultricies leo integer malesuada. Morbi tristique senectus et netus et. Nec feugiat in fermentum posuere. Vitae proin sagittis nisl rhoncus mattis. Feugiat in ante metus dictum at tempor commodo ullamcorper.
+
+Et odio pellentesque diam volutpat commodo sed. Tellus rutrum tellus pellentesque eu tincidunt tortor. Facilisi nullam vehicula ipsum a. Vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Enim blandit volutpat maecenas volutpat blandit aliquam etiam. Vel pharetra vel turpis nunc eget lorem dolor. Mollis nunc sed id semper risus. Non consectetur a erat nam at lectus urna. Non tellus orci ac auctor augue mauris augue neque. Aliquam vestibulum morbi blandit cursus risus at ultrices mi tempus. Non tellus orci ac auctor augue mauris. Amet dictum sit amet justo donec enim diam vulputate ut. Eget nunc scelerisque viverra mauris in. Sagittis id consectetur purus ut. Enim ut tellus elementum sagittis vitae et. Etiam non quam lacus suspendisse. Non sodales neque sodales ut. Aliquet enim tortor at auctor urna nunc id. Eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Tellus integer feugiat scelerisque varius morbi enim nunc faucibus.
+
+Arcu non sodales neque sodales ut etiam. Orci porta non pulvinar neque laoreet suspendisse. Aenean vel elit scelerisque mauris pellentesque. Praesent tristique magna sit amet. Sit amet est placerat in egestas erat imperdiet sed. Vestibulum lorem sed risus ultricies tristique nulla aliquet enim. Ac felis donec et odio pellentesque diam volutpat commodo sed. Nunc vel risus commodo viverra maecenas. Sodales ut etiam sit amet nisl purus in mollis. Habitant morbi tristique senectus et netus et. Suspendisse faucibus interdum posuere lorem ipsum dolor sit amet. Sagittis eu volutpat odio facilisis mauris sit amet. Pellentesque habitant morbi tristique senectus et netus et. Fermentum dui faucibus in ornare quam viverra orci sagittis eu. Eget lorem dolor sed viverra ipsum.
+
+Egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris. Euismod in pellentesque massa placerat duis ultricies lacus sed turpis. At varius vel pharetra vel turpis nunc eget lorem. Tortor aliquam nulla facilisi cras fermentum. Risus feugiat in ante metus dictum at tempor commodo. Faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Tellus orci ac auctor augue. Sit amet nisl suscipit adipiscing bibendum est. Aliquet bibendum enim facilisis gravida neque convallis. In hac habitasse platea dictumst vestibulum rhoncus.
+
+Proin nibh nisl condimentum id venenatis a condimentum. Justo nec ultrices dui sapien eget. Pulvinar mattis nunc sed blandit. Mauris in aliquam sem fringilla ut morbi tincidunt. Id ornare arcu odio ut sem nulla pharetra. Libero volutpat sed cras ornare arcu. Nunc sed blandit libero volutpat sed cras. Nunc consequat interdum varius sit. Porttitor rhoncus dolor purus non. Felis eget nunc lobortis mattis aliquam faucibus. Placerat in egestas erat imperdiet sed euismod nisi. Eu lobortis elementum nibh tellus molestie nunc non blandit. Maecenas sed enim ut sem viverra aliquet eget sit amet. Id leo in vitae turpis massa sed elementum tempus. Elit ullamcorper dignissim cras tincidunt lobortis feugiat. Fusce ut placerat orci nulla pellentesque dignissim enim. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique. Sed lectus vestibulum mattis ullamcorper velit. Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus. Etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum.
+
+Risus pretium quam vulputate dignissim suspendisse in est. Orci porta non pulvinar neque. Vitae semper quis lectus nulla at volutpat. Facilisi etiam dignissim diam quis. Ac felis donec et odio pellentesque diam volutpat commodo. Fringilla urna porttitor rhoncus dolor purus non enim praesent. Non diam phasellus vestibulum lorem sed. Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Feugiat in fermentum posuere urna nec tincidunt praesent semper feugiat. Mattis molestie a iaculis at erat pellentesque. Est placerat in egestas erat. Purus semper eget duis at tellus at.
+
+Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Id ornare arcu odio ut sem nulla pharetra. Nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. In pellentesque massa placerat duis. Facilisis mauris sit amet massa vitae tortor condimentum lacinia quis. Facilisi cras fermentum odio eu feugiat. Sagittis purus sit amet volutpat consequat mauris. Pharetra et ultrices neque ornare aenean euismod elementum nisi quis. Diam sit amet nisl suscipit adipiscing. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar. Sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum.`;
+
+$(document).ready(function() {
+
+   // Populate nav menu
+   // $('#nav_menu').append(Object.keys(pages).reduce(function (acc, title) {
+   //    return `${acc}<a href="${pages[title]}" class="nav_menu_item">${title}</a>`
+   // }, ''));
+   $('#nav_menu').append(render_links(pages, 'nav_menu_item'));
+   // console.log($('#nav_icon').css('--image'));
+   // var url = $('#nav_icon').css('--image');
+   // console.log((new URL(url)).href);
+   // $('#nav_icon').prepend($('<img>', {src: url}))
+   // $('#nav_icon').prepend(`<img src=${url}/>`)
+   //    Object.keys(pages).reduce(function (acc, title) {
+   //    return `${acc}<a href="${pages[title]}" class="nav_menu_item">${title}</a>`
+   // }, ''));
+
+   // todo: bold menu item of current page
+   //
+   // console.log('blah');
+
+
+});
+
+function render_links(obj, klass) {
+   return Object.keys(obj).reduce(function (acc, title) {
+      return `${acc}<a href="${obj[title]}" class="${klass}">${title}</a>`
+   }, '');
+}
